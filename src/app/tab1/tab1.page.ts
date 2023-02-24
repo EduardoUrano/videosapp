0584dec0b855c7db855f85044ff781b7a729b298
EduardoUrano <eduardo@mundohabitat.com.br>
2023-02-24 17:22:22 -0300

    Adicionamos o componente alert

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +8,29 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(private alertController: AlertController) {}
 
-}
+  async exibirDeletarSala() {
+    const alert = await this.alertController.create({
+      header: 'Deletar Sala?',
+      message: "Confirm",
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          handler: (blah) => {
+            console.log('Confirm Cancel: blah');
+          },
+        },
+        {
+          text: 'Deletar',
+
+          handler: () => {
+            console.log('Confirm Okay');
+          }
+        }
+      ]
+    });
+await alert.present();
+
+}}
