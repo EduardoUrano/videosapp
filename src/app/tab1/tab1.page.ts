2747330d55a7b20dc8390a5f7f62541df4beecbe
EduardoUrano <eduardo@mundohabitat.com.br>
2023-03-06 21:33:10 -0300

    testei a cor laranja mas ficou feio, deixei ela criada mais não usei

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AlertController } from '@ionic/angular';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -8,12 +9,12 @@ import { AlertController } from '@ionic/angular';
 })
 export class Tab1Page {
 
-  constructor(private alertController: AlertController) {}
+  constructor(private alertController: AlertController, private toastController: ToastController) {}
 
   async exibirDeletarSala() {
     const alert = await this.alertController.create({
       header: 'Deletar Sala?',
-      message: "Confirm",
+      message: "A sala será deletada.",
       buttons: [
         {
           text: 'Cancelar',
@@ -26,11 +27,37 @@ export class Tab1Page {
           text: 'Deletar',
 
           handler: () => {
-            console.log('Confirm Okay');
+            this.deletouToast();
           }
         }
       ]
     });
 await alert.present();
 
-}}
+}
+
+handlerMessage = '';
+roleMessage = '';
+async deletouToast() {
+  const toast = await this.toastController.create({
+    message: 'Sala Deletada',
+    duration: 3000,
+    color: 'danger',
+    buttons: [
+      {
+        text: 'Dispensar',
+        role: 'cancel',
+        handler: () => { this.handlerMessage = 'Dismiss clicked'; }
+      }
+    ]
+  });
+
+  await toast.present();
+
+  const { role } = await toast.onDidDismiss();
+  this.roleMessage = `Dismissed with role: ${role}`;
+}
+}
+
+
+
